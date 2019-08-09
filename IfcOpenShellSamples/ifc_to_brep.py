86f9481df870cda4b5617fb1a3945ce4b99cfecd
cyril@biminsight.ch <UwLLxEzxXeBoEvnVzztk>
2019-08-09 19:03:38 +0200

    Refactoring. Reduce main execution.

--- IfcOpenShellSamples/ifc_to_brep.py
+++ IfcOpenShellSamples/ifc_to_brep.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import ifcopenshell
 from ifcopenshell import geom
 import FreeCAD
@@ -5,13 +6,14 @@ import FreeCADGui
 import Part
 
 
-def read_geom(ifc_entity, doc, settings):
+def convert_geom(ifc_entity, doc, settings):
+    """Convert ifc entity to a basic FreeCAD Part"""
 
     shape = geom.create_shape(settings, ifc_entity)
-    # occ stands for OpenCascade 
+    # occ stands for OpenCascade
     occ_shape = shape.geometry.brep_data
 
-    # IfcOpenShell generate an Open Cascade BREP 
+    # IfcOpenShell generate an Open Cascade BREP
     with open("IfcOpenShellSamples/brep_data", "w") as file:
         file.write(occ_shape)
 
@@ -21,17 +23,16 @@ def read_geom(ifc_entity, doc, settings):
 
     # Ifc lenght internal unit : meter. FreeCAD internal unit : mm.
     fc_shape.scale(1000)
-    
+
     # Add geometry to FreeCAD scenegraph (Coin)
     fc_part = doc.addObject("Part::Feature", "IfcPart")
     fc_part.Shape = fc_shape
-    
-    return fc_part
-    
 
-if __name__ == "__main__":
-    # Open the IFC file
-    ifc_path = "IfcOpenShellSamples/Wall.ifc"
+    return fc_part
+
+
+def display_geom(ifc_path, base_class="IfcProduct"):
+    """Display geom in from file in FreeCAD"""
     ifc_file = ifcopenshell.open(ifc_path)
 
     # Define settings
@@ -42,13 +43,22 @@ if __name__ == "__main__":
     FreeCADGui.showMainWindow()
     doc = FreeCAD.newDocument()
 
-    parts = [read_geom(ifc_entity, doc, settings) for ifc_entity in ifc_file.by_type("IfcWall")]
+    parts = [
+        convert_geom(ifc_entity, doc, settings)
+        for ifc_entity in ifc_file.by_type(base_class)
+    ]
 
     # Set Draw Style to display mesh edges. Orient view and fit to wall
-    FreeCADGui.runCommand("Std_DrawStyle",1) 
+    FreeCADGui.runCommand("Std_DrawStyle", 1)
     for part in parts:
         FreeCADGui.Selection.addSelection(part)
     FreeCADGui.activeView().viewIsometric()
     FreeCADGui.SendMsgToActiveView("ViewSelection")
-    
+
     FreeCADGui.exec_loop()
+
+
+if __name__ == "__main__":
+    # Open the IFC file
+    ifc_path = "IfcOpenShellSamples/Wall.ifc"
+    display_geom(ifc_path, base_class="IfcWall")
